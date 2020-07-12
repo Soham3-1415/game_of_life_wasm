7131637bf17edc1c52a983f97e77a589af62783c
Soham Roy <sohamroy@sohamroy.me>
2020-07-11 22:51:13 -0500

    Separate build script for wasm-pack

--- export.sh
+++ export.sh
@@ -5,7 +5,7 @@ NAME=game_of_life_wasm
 WASM_DIR_NAME=wasm
 WASM_BUILD_DIR=static/js/"$WASM_DIR_NAME"
 
-wasm-pack build -d static/js/wasm --target web
+./build.sh
 
 [ -d export ] && rm -r export
 mkdir export
@@ -13,10 +13,17 @@ mkdir export/css
 mkdir export/js
 mkdir export/errors
 
-cp -r static/*.html export/
-cp -r static/css/*.css export/css/
-cp -r static/js/*.js export/js/
-cp -r static/errors/*.html export/errors
+mkdir export/bootstrap
+mkdir export/bootstrap/css
+mkdir export/bootstrap/js
+
+cp static/*.html export/
+cp static/css/*.css export/css/
+cp static/js/*.js export/js/
+cp static/errors/*.html export/errors
+
+cp static/bootstrap/css/*.min.css export/bootstrap/css/
+cp static/bootstrap/js/*.min.js export/bootstrap/js/
 
 mkdir export/js/"$WASM_DIR_NAME"
 cp "$WASM_BUILD_DIR"/"$NAME".js "$WASM_BUILD_DIR"/"$NAME"_bg.wasm export/js/"$WASM_DIR_NAME"/
@@ -28,12 +35,18 @@ brotli -k export/js/*.js
 brotli -k export/js/"$WASM_DIR_NAME"/*.wasm
 brotli -k export/js/"$WASM_DIR_NAME"/*.js
 
-gzip --best -k export/*.html
-gzip --best -k export/errors/*.html
-gzip --best -k export/css/*.css
-gzip --best -k export/js/*.js
-gzip --best -k export/js/"$WASM_DIR_NAME"/*.wasm
-gzip --best -k export/js/"$WASM_DIR_NAME"/*.js
+brotli -k export/bootstrap/css/*.css
+brotli -k export/bootstrap/js/*.js
+
+gzip -k export/*.html
+gzip -k export/errors/*.html
+gzip -k export/css/*.css
+gzip -k export/js/*.js
+gzip -k export/js/"$WASM_DIR_NAME"/*.wasm
+gzip -k export/js/"$WASM_DIR_NAME"/*.js
+
+gzip -k export/bootstrap/css/*.css
+gzip -k export/bootstrap/js/*.js
 
 ssh projects "rm -r /var/www/testapp/web/*"
 scp -r export/* projects:/var/www/testapp/web/
