e222f9859db2ceb0dc15c495d1a9f9787b5491c0
Soham Roy <sohamroy@sohamroy.me>
2020-07-17 15:43:29 -0500

    Removed error pages

--- www/src/bundles/app.js
+++ www/src/bundles/app.js
@@ -1,2 +1,6 @@
 import('../js/app.js')
     .catch(e => console.error('Error importing `app.js`:', e));
+
+import '../bootstrap/bootstrap.bundle.min.js';
+import '../bootstrap/bootstrap.min.css';
+import '../css/styles.css';
--- www/src/bundles/lib.js
+++
@@ -1,3 +0,0 @@
-import '../bootstrap/bootstrap.bundle.min.js';
-import '../bootstrap/bootstrap.min.css';
-import '../css/styles.css';
